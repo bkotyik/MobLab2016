9f144a64bb17eb4e2117330d208bfaf70424d532
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-11 10:19:36 +0200

    renamed building model to floorModel

--- app/src/main/java/bkotyik/mobsoft2016/model/FloorModel.java
+++ app/src/main/java/bkotyik/mobsoft2016/model/FloorModel.java
@@ -1,12 +1,12 @@
 package bkotyik.mobsoft2016.model;
 
-public class BuildingModel {
+public class FloorModel {
 
     private int id;
     private String name;
     private String description;
 
-    public BuildingModel() {
+    public FloorModel() {
 
     }
 }
