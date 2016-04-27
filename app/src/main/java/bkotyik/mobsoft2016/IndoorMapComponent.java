32584166514d50a878172e1fe7741dc456eafc05
bkotyik <bkotyik@gmail.com>
2016-04-27 17:37:52 +0200

    Fixed typo in the name of FloorEditorActivity

--- app/src/main/java/bkotyik/mobsoft2016/IndoorMapComponent.java
+++ app/src/main/java/bkotyik/mobsoft2016/IndoorMapComponent.java
@@ -9,7 +9,7 @@ import bkotyik.mobsoft2016.model.ModelModule;
 import bkotyik.mobsoft2016.presenter.MainPresenter;
 import bkotyik.mobsoft2016.view.EmployeeSearchActivity;
 import bkotyik.mobsoft2016.view.FloorDetailsActivity;
-import bkotyik.mobsoft2016.view.FloorEditorAcitivity;
+import bkotyik.mobsoft2016.view.FloorEditorActivity;
 import bkotyik.mobsoft2016.view.MainActivity;
 import bkotyik.mobsoft2016.view.ViewModule;
 import dagger.Component;
@@ -21,7 +21,7 @@ public interface IndoorMapComponent {
     void inject(MainActivity mainActivity);
     void inject(FloorDetailsActivity floorDetailsActivity);
     void inject(EmployeeSearchActivity employeeSearchActivity);
-    void inject(FloorEditorAcitivity floorEditorAcitivity);
+    void inject(FloorEditorActivity floorEditorActivity);
     void inject(MainPresenter mainPresenter);
     void inject(FloorInteractor floorInteractor);
     void inject(EmployeeInteractor employeeInteractor);
