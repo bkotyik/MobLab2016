02f9273604e25fd9ee0e3331b3d556c10453a5b9
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-05-23 08:58:24 +0200

    added google-services.json to app dir. added dependencies into build.gradle. added tracker into App

--- app/src/main/java/bkotyik/mobsoft2016/IndoorMapApplication.java
+++ app/src/main/java/bkotyik/mobsoft2016/IndoorMapApplication.java
@@ -1,12 +1,17 @@
 package bkotyik.mobsoft2016;
 
 import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 import bkotyik.mobsoft2016.view.ViewModule;
 
 
 public class IndoorMapApplication extends Application {
 
     public static IndoorMapComponent injector;
+    private Tracker mTracker;
 
     @Override
     public void onCreate() {
@@ -23,4 +28,18 @@ public class IndoorMapApplication extends Application {
         injector = appComponent;
         injector.inject(this);
     }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
+    }
+
 }
